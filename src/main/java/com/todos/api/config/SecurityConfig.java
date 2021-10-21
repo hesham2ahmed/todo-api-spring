741f4c0d46e247c92be6bd34ead27146cbc1fa91
heshmAhmed <heshamahmed22226.ha@gmail.com>
2021-10-21 04:23:06 +0200

    update

--- src/main/java/com/todos/api/config/SecurityConfig.java
+++ src/main/java/com/todos/api/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilter(corsFilter())
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
